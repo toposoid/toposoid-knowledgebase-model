f15f8ae3ac51e21454014103534f2ddd6e75aec2
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-08-17 07:39:47 +0000

    https://github.com/toposoid/toposoid-knowledgebase-model/issues/144

--- src/main/scala/com/ideal/linked/toposoid/knowledgebase/image/model/SingleImage.scala
+++ src/main/scala/com/ideal/linked/toposoid/knowledgebase/image/model/SingleImage.scala
@@ -26,7 +26,7 @@ import play.api.libs.json.{Json, OWrites, Reads}
   * - toposoid-feature-vectorizer
   * @param url
   */
-case class SingleImage(url:String)
+case class SingleImage(url:String, x:Int=0, y:Int=0, width:Int=0, height:Int=0)
 
 object SingleImage {
   implicit val jsonWrites: OWrites[SingleImage] = Json.writes[SingleImage]
